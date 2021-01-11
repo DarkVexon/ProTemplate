31bc8fa3c70f6bce18f6b9f0ddbab1012e194184
Vex Darkon <edanvirgo@gmail.com>
2021-01-10 18:07:46 -0700

    Some changes. Im so mad

---
+++ src/main/java/theTodo/cards/Strike.java
@@ -0,0 +1,25 @@
+package theTodo.cards;
+
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static theTodo.TodoMod.makeID;
+
+public class Strike extends AbstractTodoCard {
+    public final static String ID = makeID("Strike");
+    // intellij stuff attack, enemy, basic, 6, 3,  , , , 
+
+    public Strike() {
+        super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        baseDamage = 6;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.FIRE);
+    }
+
+    public void upp() {
+        upgradeDamage(3);
+    }
+}
