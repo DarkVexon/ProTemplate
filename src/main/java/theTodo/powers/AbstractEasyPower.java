8d34d2714e9ae2cbe88500d54b18338ae1432e95
DarkVexon <edanvirgo@gmail.com>
2021-01-26 18:11:01 -0700

    Fix AbstractEasyPower

--- src/main/java/theTodo/powers/AbstractEasyPower.java
+++ src/main/java/theTodo/powers/AbstractEasyPower.java
@@ -3,24 +3,41 @@ package theTodo.powers;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
 import com.megacrit.cardcrawl.powers.AbstractPower;
+import theTodo.TodoMod;
 import theTodo.util.TexLoader;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static theTodo.TodoMod.makeID;
 
 public abstract class AbstractEasyPower extends AbstractPower {
+    private static PowerStrings getPowerStrings(String ID) {
+        return CardCrawlGame.languagePack.getPowerStrings(ID);
+    }
+
+    protected static Map<String, PowerStrings> powerStrings = new HashMap<>();
+
+    protected String[] DESCRIPTIONS;
+
     public AbstractEasyPower(String NAME, PowerType powerType, boolean isTurnBased, AbstractCreature owner, int amount) {
-        this.ID = makeID(NAME.replaceAll("([ ])", "") );
+        this.ID = makeID(NAME.replaceAll("([ ])", ""));
         this.isTurnBased = isTurnBased;
 
+        if (!powerStrings.containsKey(this.ID))
+            powerStrings.put(this.ID, getPowerStrings(this.ID));
         this.name = NAME;
+        this.DESCRIPTIONS = powerStrings.get(this.ID).DESCRIPTIONS;
 
         this.owner = owner;
         this.amount = amount;
         this.type = powerType;
 
-        Texture normalTexture = TexLoader.getTexture("bronzeResources/images/powers/" + NAME.replaceAll("([ ])", "")  + "32.png");
-        Texture hiDefImage = TexLoader.getTexture("bronzeResources/images/powers/" + NAME.replaceAll("([ ])", "")  + "84.png");
+        Texture normalTexture = TexLoader.getTexture(TodoMod.modID + "Resources/images/powers/" + NAME.replaceAll("([ ])", "") + "32.png");
+        Texture hiDefImage = TexLoader.getTexture(TodoMod.modID + "Resources/images/powers/" + NAME.replaceAll("([ ])", "") + "84.png");
         if (hiDefImage != null) {
             region128 = new TextureAtlas.AtlasRegion(hiDefImage, 0, 0, hiDefImage.getWidth(), hiDefImage.getHeight());
             if (normalTexture != null)
