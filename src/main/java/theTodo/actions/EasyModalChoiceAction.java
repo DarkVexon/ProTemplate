3d0fcb0b7b3980560edad7b02a981e1f63b39629
DarkVexon <edanvirgo@gmail.com>
2021-01-27 18:16:46 -0700

    When you doing the cool thing

--- src/main/java/theTodo/actions/EasyModalChoiceAction.java
+++ src/main/java/theTodo/actions/EasyModalChoiceAction.java
@@ -1,13 +1,11 @@
 package theTodo.actions;
 
-import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsAction;
+import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsCenteredAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 
 import java.util.ArrayList;
 
-public class EasyModalChoiceAction extends SelectCardsAction {
-
-    //TODO: When the StsLib SelectCardsCenteredAction PR is pushed to steam, swap the extended action with that.
+public class EasyModalChoiceAction extends SelectCardsCenteredAction {
 
     public EasyModalChoiceAction(ArrayList<AbstractCard> list, int amount, String textforSelect) {
         super(list, amount, textforSelect, (cards) -> {
