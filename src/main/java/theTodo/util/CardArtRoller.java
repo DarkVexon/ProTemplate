0b3802e54ce6d0a1780e7fd8d1bb3ea0f945a68d
Vex Darkon <edanvirgo@gmail.com>
2022-01-24 23:16:49 -0700

    Remove unnecessaryt depenendency

--- src/main/java/theTodo/util/CardArtRoller.java
+++ src/main/java/theTodo/util/CardArtRoller.java
@@ -9,7 +9,6 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.FrameBuffer;
 import com.badlogic.gdx.graphics.glutils.ShaderProgram;
-import com.github.tommyettinger.colorful.Shaders;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.helpers.CardLibrary;
 import com.megacrit.cardcrawl.random.Random;
@@ -19,9 +18,64 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class CardArtRoller {
+    public static final String partialHueRodrigues =
+            "vec3 applyHue(vec3 rgb, float hue)\n" +
+                    "{\n" +
+                    "    vec3 k = vec3(0.57735);\n" +
+                    "    float c = cos(hue);\n" +
+                    "    //Rodrigues' rotation formula\n" +
+                    "    return rgb * c + cross(k, rgb) * sin(hue) + k * dot(k, rgb) * (1.0 - c);\n" +
+                    "}\n";
+    public static final String vertexShaderHSLC = "attribute vec4 a_position;\n"
+            + "attribute vec4 a_color;\n"
+            + "attribute vec2 a_texCoord0;\n"
+            + "uniform mat4 u_projTrans;\n"
+            + "varying vec4 v_color;\n"
+            + "varying vec2 v_texCoords;\n"
+            + "varying float v_lightFix;\n"
+            + "\n"
+            + "void main()\n"
+            + "{\n"
+            + "   v_color = a_color;\n"
+            + "   v_texCoords = a_texCoord0;\n"
+            + "   v_color.a = pow(v_color.a * (255.0/254.0) + 0.5, 1.709);\n"
+            + "   v_lightFix = 1.0 + pow(v_color.a, 1.41421356);\n"
+            + "   gl_Position =  u_projTrans * a_position;\n"
+            + "}\n";
+
+    public static final String fragmentShaderHSLC =
+            "#ifdef GL_ES\n" +
+                    "#define LOWP lowp\n" +
+                    "precision mediump float;\n" +
+                    "#else\n" +
+                    "#define LOWP \n" +
+                    "#endif\n" +
+                    "varying vec2 v_texCoords;\n" +
+                    "varying float v_lightFix;\n" +
+                    "varying LOWP vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    partialHueRodrigues +
+                    "void main()\n" +
+                    "{\n" +
+                    "    float hue = 6.2831853 * (v_color.x - 0.5);\n" +
+                    "    float saturation = v_color.y * 2.0;\n" +
+                    "    float brightness = v_color.z - 0.5;\n" +
+                    "    vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
+                    "    tgt.rgb = applyHue(tgt.rgb, hue);\n" +
+                    "    tgt.rgb = vec3(\n" +
+                    "     (0.5 * pow(dot(tgt.rgb, vec3(0.375, 0.5, 0.125)), v_color.w) * v_lightFix + brightness),\n" + // lightness
+                    "     ((tgt.r - tgt.b) * saturation),\n" + // warmth
+                    "     ((tgt.g - tgt.b) * saturation));\n" + // mildness
+                    "    gl_FragColor = clamp(vec4(\n" +
+                    "     dot(tgt.rgb, vec3(1.0, 0.625, -0.5)),\n" + // back to red
+                    "     dot(tgt.rgb, vec3(1.0, -0.375, 0.5)),\n" + // back to green
+                    "     dot(tgt.rgb, vec3(1.0, -0.375, -0.5)),\n" + // back to blue
+                    "     tgt.a), 0.0, 1.0);\n" + // keep alpha, then clamp
+                    "}";
+
     private static HashMap<String, TextureAtlas.AtlasRegion> doneCards = new HashMap<>();
     public static HashMap<String, ReskinInfo> infos = new HashMap<String, ReskinInfo>();
-    private static ShaderProgram shade = new ShaderProgram(Shaders.vertexShaderHSLC, Shaders.fragmentShaderHSLC);
+    private static ShaderProgram shade = new ShaderProgram(vertexShaderHSLC, fragmentShaderHSLC);
 
     public static void computeCard(AbstractEasyCard c) {
         c.portrait = doneCards.computeIfAbsent(c.cardID, key -> {
