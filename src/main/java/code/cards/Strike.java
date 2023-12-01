223fb84a77c7815f3eeb9be39cceef91029fd739
Ocean <ocean@ocean.lol>
2023-12-01 03:10:03 +0000

    update s&d

--- src/main/java/code/cards/Strike.java
+++ src/main/java/code/cards/Strike.java
@@ -12,16 +12,12 @@ public class Strike extends AbstractEasyCard {
 
     public Strike() {
         super(ID, 1, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
-        baseDamage = 6;
+        setDamage(6, +3);
         tags.add(CardTags.STRIKE);
         tags.add(CardTags.STARTER_STRIKE);
     }
 
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.NONE);
-    }
-
-    public void upp() {
-        upgradeDamage(3);
     }
 }
